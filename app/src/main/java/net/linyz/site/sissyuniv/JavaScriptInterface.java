cb57e7ea0a88e01054ecf1dd7583a60619ac8427
LIN Yizhou <yizhou.lin@outlook.com>
2020-08-14 10:32:12 +0800

    first commit

---
+++ app/src/main/java/net/linyz/site/sissyuniv/JavaScriptInterface.java
@@ -0,0 +1,79 @@
+package net.linyz.site.sissyuniv;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Base64;
+import android.webkit.JavascriptInterface;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class JavaScriptInterface {
+    private Context context;
+
+    public JavaScriptInterface(Context context) {
+        this.context = context;
+    }
+
+    @JavascriptInterface
+    public void getBase64FromBlobData(String base64Data) {
+        try {
+            convertBase64StringToPdfAndStoreIt(base64Data);
+        } catch (IOException e) {
+            Toast.makeText(context, context.getString(R.string.save_failed_hint), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static String getBase64StringFromBlobUrl(String blobUrl) {
+        if (blobUrl.startsWith("blob")) {
+            return "javascript: var xhr = new XMLHttpRequest();" +
+                    "xhr.open('GET', '" + blobUrl + "', true);" +
+                    "xhr.setRequestHeader('Content-type','application/octet-stream');" +
+                    "xhr.responseType = 'blob';" +
+                    "xhr.onload = function(e) {" +
+                    "    if (this.status == 200) {" +
+                    "        var blobPdf = this.response;" +
+                    "        var reader = new FileReader();" +
+                    "        reader.readAsDataURL(blobPdf);" +
+                    "        reader.onloadend = function() {" +
+                    "            base64data = reader.result;" +
+                    "            Android.getBase64FromBlobData(base64data);" +
+                    "        }" +
+                    "    }" +
+                    "};" +
+                    "xhr.send();";
+        }
+        return "javascript: console.log('It is not a Blob URL');";
+    }
+
+    private void convertBase64StringToPdfAndStoreIt(String base64PDf) throws IOException {
+        final int notificationId = 1;
+        SimpleDateFormat dateFmt= new SimpleDateFormat("yyyy-MM-dd_HHmmss");
+        String currentDateTime = dateFmt.format(new Date());
+        final File dwldsPath = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS) + "/suSave_" + currentDateTime + "_.suSave");
+        byte[] pdfAsBytes = Base64.decode(base64PDf.replaceFirst("^data:.{0,100};base64,", ""), 0);
+        FileOutputStream os;
+        os = new FileOutputStream(dwldsPath, false);
+        os.write(pdfAsBytes);
+        os.flush();
+        Toast.makeText(context, "Save to " + dwldsPath.getPath(), Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/net/linyz/site/sissyuniv/MainActivity.java
@@ -0,0 +1,140 @@
+package net.linyz.site.sissyuniv;
+
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.DownloadListener;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    WebView mainWebView;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (mainWebView != null) {
+                    mainWebView.loadUrl(getBaseContext().getString(R.string.inedx_page_url));
+                }
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void askForPermissions(){
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                },
+                201);
+    }
+
+    private void setupWebSettings(WebSettings webSettings){
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setSupportMultipleWindows(true);
+        webSettings.setBuiltInZoomControls(false);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setAppCachePath("");
+        webSettings.setDatabaseEnabled(true);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        askForPermissions();
+
+        setContentView(R.layout.activity_main);
+
+        mainWebView = findViewById(R.id.main_web);
+
+        WebSettings webSettings = mainWebView.getSettings();
+        setupWebSettings(webSettings);
+
+        // Set js interface and downloadListener for blob download in progress saving
+        mainWebView.addJavascriptInterface(new JavaScriptInterface(getBaseContext()), "Android");
+        mainWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+                mainWebView.loadUrl(JavaScriptInterface.getBase64StringFromBlobUrl(url));
+            }
+        });
+
+        // Set on loading behavior use webview for local files and default browser for http(s) links.
+        mainWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String url = request.getUrl().toString();
+                Log.d("WebView", "Attempting to load URL: " + url);
+                if (url.startsWith("http://") || url.startsWith("https://")) {
+                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    return true;
+                }
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        // Setup file picker for progress loading
+        mainWebView.setWebChromeClient(new WebChromeClient() {
+            // For Lollipop 5.0+ Devices
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e) {
+                    uploadMessage = null;
+                    Toast.makeText(getApplicationContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+        });
+
+        // Load page
+        mainWebView.loadUrl(getBaseContext().getString(R.string.inedx_page_url));
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (requestCode == REQUEST_SELECT_FILE) {
+            if (uploadMessage == null)
+                return;
+            uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+            uploadMessage = null;
+        }
+    }
+}
